80babd2168daf0cdf85eff487de65c5fc3838eee
cooray <corycarlson209@gmail.com>
2018-02-14 17:11:59 -0600

    fixed app.listen for prod.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+const PORT = process.env.PORT || 5000
 
 // viewed at http://localhost:8080
 app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.listen(8080, function(){
-    console.log('listening on port 8080');
-});
+app.listen(PORT, ()=> console.log('listening on port ' + PORT));
