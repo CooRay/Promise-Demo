42dc868cdb0c1295eae2c9cb6b218c92943b5708
cooray <corycarlson209@gmail.com>
2018-02-14 17:21:30 -0600

    cleanup server.js

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var app = express();
 var path = require('path');
 const PORT = process.env.PORT || 5000
 
-// viewed at http://localhost:8080
 app.use(express.static('public'));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname + '/index.html')));
+// viewed at http://localhost:5000
 app.listen(PORT, ()=> console.log('listening on port ' + PORT));
